3dd26d6a0688c8330547d026b7540d24cc79bf7e
AILISOS <ailisos17@gmail.com>
2024-03-14 18:44:41 +0000

    Create private meetings, send invitation emails, buttons

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,34 +1,40 @@
-"use server"
+"use server";
 
-import { currentUser } from "@clerk/nextjs";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { StreamClient } from "@stream-io/node-sdk";
 
 export async function getToken() {
-    const streamApiKey = process.env.NEXT_PUBLIC_STTREAM_VIDEO_API_KEY
-    const streamApiSecret = process.env.STREAM_VIDEO_API_SECRET
+  const streamApiKey = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY;
+  const streamApiSecret = process.env.STREAM_VIDEO_API_SECRET;
 
+  if (!streamApiKey || !streamApiSecret) {
+    throw new Error("Stream API key or secret not set");
+  }
 
-    if(!streamApiKey || !streamApiSecret) {
-        throw new Error("Stream API key or token nnot set");
-    }
+  const user = await currentUser();
 
-    const user = await currentUser();
+  console.log("Generating token for user: ", user?.id);
 
-    console.log("Generating token for user: ", user?.id);
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
 
-    if (!user) {
-        throw new Error("User not authennticated");
-    }
+  const streamClient = new StreamClient(streamApiKey, streamApiSecret);
 
-    const streamClient = new StreamClient(streamApiKey, streamApiSecret);
+  const expirationTime = Math.floor(Date.now() / 1000) + 60 * 60;
 
-    const expirationTime = Math.floor(Date.now() / 1000) + 60 * 60;
+  const issuedAt = Math.floor(Date.now() / 1000) - 60;
 
-    const issuedAt = Math.floor(Date.now() / 1000) - 60;
+  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
 
-    const token = streamClient.createToken(user.id, expirationTime , issuedAt);
+  console.log("Successfully generated token: ", token);
 
-    console.log("Successfully generated token: ", token);
+  return token;
+}
 
-    return token;
+export async function getUserIds(emailAddresses: string[]) {
+  const response = await clerkClient.users.getUserList({
+    emailAddress: emailAddresses,
+  });
+  return response.map((user) => user.id);
 }
